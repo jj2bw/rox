d8d999a9fe338c3535c0b2e41110753b1004635f
jj2bw <226866852+jj2bw@users.noreply.github.com>
2025-08-16 21:15:13 +0200

    fix: duplicate line

--- src/Entity/Member.php
+++ src/Entity/Member.php
@@ -27,7 +27,6 @@ use Scheb\TwoFactorBundle\Model\Totp\TwoFactorInterface as TotpTwoFactorInterfac
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherAwareInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @SuppressWarnings("PHPMD")
